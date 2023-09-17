5418cf3414e4d8564897d7f0865237be5b94df59
FrankBedoyaMejia <franksurenuga@gmail.com>
2023-09-17 13:50:42 -0500

    Maquetacion de seccion principal realizada

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { React } from "react";
 import Header from "./components/Header";
-import SeccionPrincipal from "./components/seccionPrincipal";
+import SeccionPrincipal from "./components/SeccionPrincipal";
 
 function App() {
   return (
--- src/components/SeccionPrincipal.jsx
+++ src/components/SeccionPrincipal.jsx
@@ -3,7 +3,15 @@ import React from "react";
 function SeccionPrincipal() {
   return (
     <>
-      <div>Hola</div>
+      <main className="main">
+        <section className="img">
+          <img src="/src/img/Frank.jpg" alt="Imagen prinicipal" />
+        </section>
+        <section>
+          <h1>Developer</h1>
+          <h3>Frank Bedoya hace tus ideas realidad</h3>
+        </section>
+      </main>
     </>
   );
 }
