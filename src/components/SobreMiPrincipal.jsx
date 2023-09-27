4235239fae784e483ded38b05191f368633c672a
FrankBedoyac4 <franksurenuga@gmail.com>
2023-09-27 18:24:41 -0500

    commit de ayer que se me olvido subirlo

--- src/components/SobreMiPrincipal.jsx
+++ src/components/SobreMiPrincipal.jsx
@@ -3,23 +3,26 @@ import React from "react";
 export const SobreMiPrincipal = () => {
   return (
     <>
-      <div className="contenedor">
-        <h1>Sobre mi.</h1>
-        <h4>
-          Soy un desarrollador web tanto frontend como backend de Medellin,
-          Colombia.
-        </h4>
-        <p>
-          Desde muy pequeño me atrajo mucho todo lo que tenia que ver con la
-          tecnologia, me gustaba demasiado ver videos, leer, aprender todo lo
-          que puediera en mis ratos libres sobre tecnologia.Mientras fui
-          creciendo asi mismo mis ganas de aprender y en este trayecto un dia
-          llegaron los videojuegos y con los videojuegos aprendi sobre la
-          programaciion, despues que me gradue me gane una beca para estudiar y
-          entrar al fin al mundo del desarrollo, luego de esto aqui estoy con
-          esta pasion que es la tecnologias mas viva que nunca y aprendiendo dia
-          a dia sobre el desarrollo y su inmenso mundo.
-        </p>
+      <div className="contenedorSobreMi">
+        <div className="letras">
+          <h1>Sobre mi.</h1>
+          <h4>
+            Soy un desarrollador web tanto frontend como backend de Medellin,
+            Colombia.
+          </h4>
+          <p>
+            Desde muy pequeño me atrajo mucho todo lo que tenia que ver con la
+            tecnologia, me gustaba demasiado ver videos, leer, aprender todo lo
+            que puediera en mis ratos libres sobre tecnologia.Mientras fui
+            creciendo asi mismo mis ganas de aprender y en este trayecto un dia
+            llegaron los videojuegos y con los videojuegos aprendi sobre la
+            programaciion, despues que me gradue me gane una beca para estudiar
+            y entrar al fin al mundo del desarrollo, luego de esto aqui estoy
+            con esta pasion que es la tecnologia mas viva que nunca y
+            aprendiendo dia a dia sobre el desarrollo y su inmenso mundo.
+          </p>
+        </div>
+        <img src="/src/img/IMG-20230323-WA0002.jpg" alt="imagenSobreMi" />
       </div>
     </>
   );
