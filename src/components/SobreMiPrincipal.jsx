538131e1b17d00950c9e2569ae06317c9bac3a66
FrankBedoyac4 <franksurenuga@gmail.com>
2024-02-15 21:03:53 -0500

    Retomando proyecto con todas las ganas

--- src/components/SobreMiPrincipal.jsx
+++ src/components/SobreMiPrincipal.jsx
@@ -13,13 +13,13 @@ export const SobreMiPrincipal = () => {
           <p>
             Desde muy pequeño me atrajo mucho todo lo que tenia que ver con la
             tecnologia, me gustaba demasiado ver videos, leer, aprender todo lo
-            que puediera en mis ratos libres sobre tecnologia.Mientras fui
+            que pudiera en mis ratos libres sobre tecnologia.Mientras fui
             creciendo asi mismo mis ganas de aprender y en este trayecto un dia
             llegaron los videojuegos y con los videojuegos aprendi sobre la
-            programaciion, despues que me gradue me gane una beca para estudiar
-            y entrar al fin al mundo del desarrollo, luego de esto aqui estoy
-            con esta pasion que es la tecnologia mas viva que nunca y
-            aprendiendo dia a dia sobre el desarrollo y su inmenso mundo.
+            programacion, despues que me gradue me gane una beca para estudiar y
+            entrar al fin al mundo del desarrollo, luego de esto aqui estoy con
+            esta pasion que es la tecnologia mas viva que nunca y aprendiendo
+            dia a dia sobre el desarrollo y su inmenso mundo..
           </p>
         </div>
         <img src="/src/img/IMG-20230323-WA0002.jpg" alt="imagenSobreMi" />
